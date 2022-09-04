8af9eb8bdbbe876c30a135af9f2fdd3bb2cac7cd
Everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-09-04 18:57:57 -0500

    aplicacion actualizada

--- src/components/componetA.jsx
+++ src/components/componetA.jsx
@@ -3,7 +3,7 @@ import {Contacto} from '../modelo/contacto'
 import ComponetB from './componetB'
 
 function componetA() {
-    const parametros = new Contacto('Robert', 'White', 'gratis@gratis.com', true);
+    const parametros = new Contacto('Robert E.', 'White collin', 'gratis@gratis.com', false);
   return (
     <div>
         <h2>Datos de contacto</h2>
